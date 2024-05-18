5f23d8a5ec588ac4eaa1aa7f8c94cc403871c3b7
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2024-05-18 14:40:27 +0200

    working on the switch to hire buttom

---
+++ hire-button.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#redirectSwitch').change(function() {
+      if ($(this).is(':checked')) {
+        
+        window.location.href = 'test.html';
+      }
+    });
+  });
