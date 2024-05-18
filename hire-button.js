88e6ca9ed702dadb49fb42a504c603d114473d9d
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2024-05-18 15:40:30 +0200

    deleting the switch-form.js since it was a simple cod instead of creating a file for it ,i've put it in the html code

--- hire-button.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#redirectSwitch').change(function() {
-      if ($(this).is(':checked')) {
-        
-        window.location.href = 'switch-form.html';
-      }
-    });
-  });
